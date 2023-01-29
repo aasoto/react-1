77b2dfc3def93e5bbcfd425c2d65389914710a21
aasoto <andresalfredosotosuarez@gmail.com>
2023-01-29 12:59:35 -0500

    gif app evento al padre y nombres unicos

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,16 +3,21 @@ import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  const onAddCategory = () => {
-    setCategories(['Valorant', ...categories]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    
+    setCategories([newCategory, ...categories]);
   }
 
   return (
     <>
       <h1>Gif Expert App</h1>
-      <AddCategory setCategories={setCategories}/>
+      <AddCategory 
+        // setCategories={setCategories}
+        onNewCategory={event => onAddCategory(event)}
+      />
       <button onClick={onAddCategory}>Agregar categoría</button>
       <ol>
         {
