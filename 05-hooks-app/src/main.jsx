fcfd6f784699f77d5d7f61e425ac9923316a6db0
aasoto <andresalfredosotosuarez@gmail.com>
2023-01-31 21:40:21 -0500

    useRef and useLayoutEffect

--- 05-hooks-app/src/main.jsx
+++ 05-hooks-app/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { Layout } from './05-useLayoutEffect/Layout'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterWithCoustomHook } from './01-useState/CounterWithCoustomHook'
@@ -10,6 +12,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <Layout />
   // </React.StrictMode>,
 )
