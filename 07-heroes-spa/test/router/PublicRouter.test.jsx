bcfea7bee8e83d7ecd8d19bc2522292c54be9994
aasoto <andresalfredosotosuarez@gmail.com>
2023-02-06 22:02:48 -0500

    debe navegar si está autenticado <PublicRoute/>

--- 07-heroes-spa/test/router/PublicRouter.test.jsx
+++ 07-heroes-spa/test/router/PublicRouter.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { AuthContext } from "../../src/auth";
 import { PublicRoute } from "../../src/router/PublicRoute";
 
@@ -27,4 +28,33 @@ describe('Pruebas de <PublicRouter />', () => {
     expect(screen.getByText('Ruta pública')).toBeTruthy();
   });
 
+  test('debe navegar si está autenticado', () => {
+
+    const contextValue = {
+      logged: true
+    }
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        {/** Si un componente renderiza el <Navigate/> este debe ser definido dentro de un MemoryRouter que hace de emulador del BrowserRouter */}
+        <MemoryRouter initialEntries={['/login']}>
+          {/** Dentro del MemoryRouter se deben definir las rutas de la prueba.
+           * Estas pueden ser rutas diferentes a las que se tiene
+           */}
+          <Routes>
+            <Route path="login" element={
+              <PublicRoute>
+                <h1>Ruta pública</h1>
+              </PublicRoute>
+            } />
+            <Route path="marvel" element={ <h1>Página Marvel</h1> } />
+          </Routes>
+
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    expect(screen.getByText('Página Marvel')).toBeTruthy();
+
+  });
 });
