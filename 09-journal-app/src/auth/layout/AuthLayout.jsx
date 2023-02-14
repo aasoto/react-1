4f8ecc05dc8e8a2777ce898e8318cbd6f6714f44
aasoto <andresalfredosotosuarez@gmail.com>
2023-02-14 18:19:07 -0500

    acomodo AuthLayout

--- 09-journal-app/src/auth/layout/AuthLayout.jsx
+++ 09-journal-app/src/auth/layout/AuthLayout.jsx
@@ -2,20 +2,19 @@ import {Grid, Typography} from '@mui/material';
 
 export const AuthLayout = ({children, title = ''}) => {
   return (
-    <Grid container spacing={ 0 } direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-  >
+    <Grid container spacing={ 0 } direction="column" alignItems="center" justifyContent="center" sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }} >
 
-    <Grid item
-      className='box-shadow'
-      xs={ 3 } //número de columnas que va a ocupar en el grid
-      sx={{ width: {sm: 450}, backgroundColor: 'white', padding: 3, borderRadius: 2 }} //esto significa styles xtra
-    >
+      <Grid item
+        className='box-shadow'
+        xs={ 3 } //número de columnas que va a ocupar en el grid
+        sx={{ width: {sm: 450}, backgroundColor: 'white', padding: 3, borderRadius: 2 }} //esto significa styles xtra
+      >
 
-      <Typography variant='h5' sx={{mb: 1}}>{title}</Typography>
+        <Typography variant='h5' sx={{mb: 1}}>{title}</Typography>
 
-      {children}
+        {children}
 
+      </Grid>
     </Grid>
-  </Grid>
   )
 }
