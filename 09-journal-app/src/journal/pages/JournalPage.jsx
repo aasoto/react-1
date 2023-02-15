562d55d5529e67af066045ba66ee34bd39e569d4
aasoto <andresalfredosotosuarez@gmail.com>
2023-02-14 21:22:40 -0500

    JournalLayout

--- 09-journal-app/src/journal/pages/JournalPage.jsx
+++ 09-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import {Typography} from '@mui/material'
 import {MailOutline} from '@mui/icons-material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto minus quibusdam aliquam maxime quos earum ullam repellendus, impedit aspernatur tempore mollitia quasi ut sint iste, officia delectus. Sit, adipisci fuga.
+      </Typography>
+    </JournalLayout>
   )
 }
