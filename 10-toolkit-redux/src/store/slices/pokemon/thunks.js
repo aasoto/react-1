208ea75fef0f739a6e8ea406e711270de960b0d4
aasoto <andresalfredosotosuarez@gmail.com>
2023-02-15 22:48:01 -0500

    primeros pasos con redux

---
+++ 10-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -0,0 +1,14 @@
+import { pokemonApi } from "../../../api/pokemonApi";
+import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
+
+export const getPokemons = (page = 0) => {
+  return async( dispatch, getState ) => {
+    dispatch( startLoadingPokemons() );
+
+    // TODO: realizar petición http
+
+    const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page}`);
+    
+    dispatch( setPokemons({ pokemons: data.results, page: page + 1 }) );
+  }
+}
