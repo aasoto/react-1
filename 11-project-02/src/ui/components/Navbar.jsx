6bf89c8a374bf5ede664252cbe110572794e5ca9
aasoto <andresalfredosotosuarez@gmail.com>
2023-02-22 12:47:52 -0500

    navbar completo

--- 11-project-02/src/ui/components/Navbar.jsx
+++ 11-project-02/src/ui/components/Navbar.jsx
@@ -1,10 +1,19 @@
-import ShoppingBag from "../icons/ShoppingBag"
+import { ShoppingBag, ShoppingCart, UserCircle } from "../icons"
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <nav>
-        <ShoppingBag size="md"/>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-5">
+            <ShoppingBag size="md"/>
+            <span className="text-black font-bold text-2xl italic">Shopping Store</span>
+          </div>
+          <div className="flex items-center gap-5">
+            <ShoppingCart size="md" />
+            <UserCircle size="md"/>
+          </div>
+        </div>
       </nav>
     </div>
   )
